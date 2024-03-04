75b5d6384eacd37a2bb1a41b9919ea95819bbc69
lindseybeidatsch <lindseyking@uchicago.edu>
2024-03-04 13:37:19 -0600

    updated scripts

--- FLDM.R
+++ FLDM.R
@@ -1,41 +1,64 @@
-library(tidyverse)
-library(papaja)
-require(base)
 
-### Assignment #2
+# read dataset from csv file 
+FLDM_tidy_long <- read.csv("FLDM_tidy.csv") %>%
+  # remove extraneous columns
+  select(-c(order, l2.mean.skills, l2.years, age, gender)) %>% 
+  # remove outlier row
+  slice(-c(74)) %>% 
+  # make data longer by having dilemma type as its own column 
+  pivot_longer(cols = c("trolley.choice", "footbridge.choice", "non.moral.choice"),
+               names_to = "dilemma.type",
+               values_to = "dilemma.choice")
 
-## ASSIGN VARIABLES
-# assign numeric variable
-age <- 23.37
 
-# assign string variable
-bday <- "August 21, 2000"
+FLDM_tidy_long$language.group <- as.factor(FLDM_tidy_long$language.group)
 
-## CREATE DATAFRAME
-# use above variables
-age_df <- data.frame(age, bday)
 
-print(age_df)
+# add zeros before id numbers with stringr to make each id number 3 digits long (consistency)
+FLDM_tidy_long$ID <- str_pad(FLDM_tidy_long$ID, 3, pad = '0') 
 
-### Assignment #4 "Hello world"
 
-hello_world <- function(selected_language) {
-  # set supported languages
-  languages <- list("Spanish", "English", "French", "German", "Italian") 
-  # set correlating greetings
-  greetings <- list("Hola mundo", "Hello world", "Bonjour le monde", "Hallo welt", "Ciao mondo") 
-  
-  if (selected_language %in% languages) { # if the language the user provides is in list "languages"
-    index <- match(selected_language, languages) # match the provided language to the list "languages"
-    greeting <- greetings[index] # index list "greetings" so the # greeting matches the # language 
-    paste0(greeting, "!") # paste the appropriate greeting based on the provided language 
-  } else {
-    "Language not supported!" # lets user know to input a different language 
-  }
-}
+# export dataframe to a csv file
+write_csv(FLDM_tidy_long, "~/Desktop/UCHI Q2/d2m/foreign-language-decisions/FLDM_tidy_long.csv") 
 
-# Call the function with the desired language
-hello_world("English")
+FLDM_German <- FLDM_tidy_long %>% 
+  filter(language.group != 5)
+
+
+FLDM_English <- FLDM_tidy_long %>% 
+  filter(language.group != 3)
+
+
+
+
+# df for demographics table 
+Condition <- c('Italian', 'German', 'English')
+N <- c(39, 28, 36)
+trolley.means <- c('54%','73%','61%')
+footbridge.means <- c('13%', '43%', '36%')
+non.moral.means <- c('87%', '92%', '93%')
+FLDM_demographics <- data.frame(Condition, N, trolley.means, footbridge.means, non.moral.means)
+
+
+## conduct descriptive analysis 
+FLDM_data <- read.csv("S1_Dataset.csv")
+
+FLDM_tidy <- FLDM_data %>% 
+  # remove unwanted columns
+  select(-c(l2.conversation, l2.reading, l2.understanding, l2.writing)) %>% 
+  #arrange the ID numbers in ascending order
+  arrange(ID)
+
+# select only the necessary groups
+FLDM_desc <- FLDM_tidy %>% 
+  select(language.group, trolley.choice, footbridge.choice, non.moral.choice) %>% 
+  drop_na() 
+
+# make longer for sake of dplyr
+FLDM_desc_long <- FLDM_desc %>% 
+  pivot_longer(c(trolley.choice, footbridge.choice, non.moral.choice), names_to = "dilemma.type")
+
+
 
   
     
--- data-prep.R
+++ data-prep.R
@@ -11,9 +11,18 @@ FLDM_tidy_long <- read.csv("FLDM_tidy.csv") %>%
                names_to = "dilemma.type",
                values_to = "dilemma.choice")
 
+library(tidyverse)
+# create new tidy dataframe from original dataframe
+FLDM_tidy <- FLDM_data %>% 
+  # remove unwanted columns
+  select(-c(l2.conversation, l2.reading, l2.understanding, l2.writing)) %>% 
+  #arrange the ID numbers in ascending order
+  arrange(ID)
+
+# export tidy dataframe to a csv file
+write_csv(FLDM_tidy, "~/Desktop/UCHI Q2/d2m/foreign-language-decisions/FLDM_tidy.csv") 
+
 FLDM_tidy_long$language.group <- as.factor(FLDM_tidy_long$language.group)
-FLDM_tidy_long$dilemma.choice<-replace(FLDM_tidy_long$dilemma.choice, FLDM_tidy_long$dilemma.choice==2, 1) 
-FLDM_tidy_long$dilemma.choice<-replace(FLDM_tidy_long$dilemma.choice, FLDM_tidy_long$dilemma.choice==1, 0) 
 View(FLDM_tidy_long)
   
 # add zeros before id numbers with stringr to make each id number 3 digits long (consistency)
@@ -58,3 +67,62 @@ footbridge.means <- c('13%', '43%', '36%')
 non.moral.means <- c('87%', '92%', '93%')
 FLDM_demographics <- data.frame(Condition, N, trolley.means, footbridge.means, non.moral.means)
 print(FLDM_demographics)
+
+## Assignment 16 conduct descriptive analysis 
+
+# select only the necessary groups
+FLDM_desc <- FLDM_data %>% 
+  select(language.group, trolley.choice, footbridge.choice, non.moral.choice) %>% 
+  drop_na() 
+
+# make longer for sake of dplyr
+FLDM_desc_long <- FLDM_desc %>% 
+  pivot_longer(c(trolley.choice, footbridge.choice, non.moral.choice), names_to = "dilemma.type")
+
+
+# deleted chunks safe keeping 
+
+```{r long data}
+library(tidyverse)
+library(reshape2)
+# read dataset from csv file 
+FLDM_tidy_long <- read.csv("FLDM_tidy.csv") %>%
+  # remove extraneous columns
+  select(-c(order, l2.mean.skills, l2.years, age, gender)) %>% 
+  # remove outlier row
+  slice(-c(74)) %>% 
+  # make data longer by having dilemma type as its own column 
+  pivot_longer(cols = c("trolley.choice", "footbridge.choice", "non.moral.choice"),
+               names_to = "dilemma.type",
+               values_to = "dilemma.choice")
+
+FLDM_tidy_long$language.group <- as.factor(FLDM_tidy_long$language.group)
+
+# add zeros before id numbers with stringr to make each id number 3 digits long (consistency)
+FLDM_tidy_long$ID <- str_pad(FLDM_tidy_long$ID, 3, pad = '0') 
+
+FLDM_German <- FLDM_tidy_long %>% 
+  filter(language.group != 5)
+
+FLDM_English <- FLDM_tidy_long %>% 
+  filter(language.group != 3)
+```
+
+```{r data read-in}
+# read dataset from csv file into rmd
+FLDM_data <- read.csv("S1_Dataset.csv")
+```
+
+
+```{r tidy data}
+library(tidyverse)
+# create new tidy dataframe from original dataframe
+FLDM_tidy <- FLDM_data %>% 
+  # remove unwanted columns
+  select(-c(l2.conversation, l2.reading, l2.understanding, l2.writing)) %>% 
+  #arrange the ID numbers in ascending order
+  arrange(ID)
+
+# export tidy dataframe to a csv file
+write_csv(FLDM_tidy, "~/Desktop/UCHI Q2/d2m/foreign-language-decisions/FLDM_tidy.csv") 
+```
